a8cfcbbd0b0908e88058db962066e1b7f57c671e
Peter Short <peter.short@gmail.com>
2017-12-29 14:32:06 +0000

    textures + fornts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate sdl2;
 
-mod test;
 mod game_of_life;
 
 use sdl2::rect::{Rect, Point};
@@ -8,92 +7,22 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::mouse::MouseButton;
 use sdl2::keyboard::Keycode;
+use sdl2::image::{ LoadTexture, INIT_JPG, INIT_PNG };
 use sdl2::video::{Window, WindowContext};
-use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::render::{Canvas, Texture, TextureCreator, TextureQuery};
 
 use game_of_life::{ State, GameOfLife, SQUARE_SIZE, PLAYGROUND_WIDTH, PLAYGROUND_HEIGHT };
 
-fn dummy_texture<'a>(
-    canvas: &mut Canvas<Window>, 
-    texture_creator: &'a TextureCreator<WindowContext>
-    ) -> (Texture<'a>, Texture<'a>) {
-
-    enum TextureColor {
-        Yellow,
-        White,
-    };
-
-    let mut square_texture1: Texture = 
-        texture_creator.create_texture_target(None, SQUARE_SIZE, SQUARE_SIZE).unwrap();
-    let mut square_texture2: Texture = 
-        texture_creator.create_texture_target(None, SQUARE_SIZE, SQUARE_SIZE).unwrap();
-    
-    {
-        let textures = vec![
-            (&mut square_texture1, TextureColor::Yellow),
-            (&mut square_texture2, TextureColor::White)
-        ];
-
-        canvas.with_multiple_texture_canvas(textures.iter(), |texture_canvas, user_context| {
-            texture_canvas.set_draw_color(Color::RGB(0, 0, 0));
-            texture_canvas.clear();
-            match *user_context {
-                TextureColor::Yellow => {
-                    for i in 0..SQUARE_SIZE {
-                        for j in 0..SQUARE_SIZE {
-                            if (i+j) % 4 == 0 {
-                                texture_canvas.set_draw_color(Color::RGB(255, 255, 0));
-                                texture_canvas.draw_point(Point::new(i as i32, j as i32)).unwrap();
-                            }
-                            if (i+j*2) % 9 == 0 {
-                                texture_canvas.set_draw_color(Color::RGB(200, 200, 0));
-                                texture_canvas.draw_point(Point::new(i as i32, j as i32)).unwrap();
-                            }
-                        }
-                    }
-                },
-                TextureColor::White => {
-                    for i in 0..SQUARE_SIZE {
-                        for j in 0..SQUARE_SIZE {
-                            if (i+j) % 7 == 0 {
-                                texture_canvas.set_draw_color(Color::RGB(192, 192, 192));
-                                texture_canvas.draw_point(Point::new(i as i32, j as i32)).unwrap();
-                            }
-                            if (i+j*2) % 5 == 0 {
-                                texture_canvas.set_draw_color(Color::RGB(64, 64, 64));
-                                texture_canvas.draw_point(Point::new(i as i32, j as i32)).unwrap();
-                            }
-                        }
-                    }
-                }
-            };
-            for i in 0..SQUARE_SIZE {
-                for j in 0..SQUARE_SIZE {
-                    if (i+j) % 7 == 0 {
-                        texture_canvas.set_draw_color(Color::RGB(192, 192, 192));
-                        texture_canvas.draw_point(Point::new(i as i32, j as i32)).unwrap();
-                    }
-                    if (i+j*2) % 5 == 0 {
-                        texture_canvas.set_draw_color(Color::RGB(64, 64, 64));
-                        texture_canvas.draw_point(Point::new(i as i32, j as i32)).unwrap();
-                    }
-                }
-            }
-        }).unwrap();
-    }
-    (square_texture1, square_texture2)
-}
-
 fn main() {
-
-    let test_t1 = test::t1::new(false);
-    let res = test_t1.get();
-    println!("Starting {}", res);
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    let _image_context = sdl2::image::init(INIT_PNG | INIT_JPG).unwrap();
+
+    let ttf_context = sdl2::ttf::init().unwrap();
+
     let window = video_subsystem
-            .window("GOL", 
+            .window("GOL",
                     SQUARE_SIZE*PLAYGROUND_WIDTH,
                     SQUARE_SIZE*PLAYGROUND_HEIGHT)
             .position_centered()
@@ -113,12 +42,19 @@ fn main() {
 
     let texture_creator: TextureCreator<_> = canvas.texture_creator();
 
-    // set up game
-    let (square_texture1, square_texture2) = dummy_texture(&mut canvas, &texture_creator);
+    let mut font = ttf_context.load_font("resource//font//DroidSans.ttf", 20).unwrap();
+
+    let bg = texture_creator.load_texture("resource//img//bg.png").unwrap();
+    let square_texture1 = texture_creator.load_texture("resource//img//one.png").unwrap();
+    let square_texture2 = texture_creator.load_texture("resource//img//two.png").unwrap();
+    let slat = texture_creator.load_texture("resource//img//slat.png").unwrap();
+
     let mut game = game_of_life::GameOfLife::new();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut frame: u32 = 0;
+
+    let mut ticks: u32 = 0;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -144,10 +80,9 @@ fn main() {
         if frame >= 30 {
             game.update();
             frame = 0;
-            println!("GOL TICK");
+            ticks += 1;
         }
 
-        //drawe
         canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
         for (i, unit) in (&game).into_iter().enumerate() {
@@ -157,15 +92,30 @@ fn main() {
             } else {
                 &square_texture2
             };
+
+            let x = ((i % PLAYGROUND_WIDTH) * SQUARE_SIZE) as i32;
+            let y = ((i / PLAYGROUND_WIDTH) * SQUARE_SIZE) as i32;
+
             if *unit {
                 canvas.copy(square_texture,
                             None,
-                            Rect::new(((i % PLAYGROUND_WIDTH) * SQUARE_SIZE) as i32,
-                                      ((i / PLAYGROUND_WIDTH) * SQUARE_SIZE) as i32,
+                            Rect::new(x, y, 
                                       SQUARE_SIZE,
                                       SQUARE_SIZE)).unwrap();
+            } else {
+                canvas.copy(&bg,
+                            None,
+                            Rect::new(x, y, SQUARE_SIZE, SQUARE_SIZE)).unwrap();
             }
         }
+
+        let surface = font.render(&format!("Game ticks = {}", ticks)).blended(Color::RGBA(255, 0, 0, 255)).unwrap();
+        let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+        let TextureQuery { width, height, .. } = texture.query();
+
+        canvas.copy(&slat, None, Rect::new(10-3, 10-3, width+6, height+6)).unwrap();
+        canvas.copy(&texture, None, Rect::new(10, 10, width, height)).unwrap();
+
         canvas.present();
 
         if let game_of_life::State::Playing = game.state() {
